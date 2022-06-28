650e943511bad066d410e075963935072ae07115
Bartek Konarowski <konarowski.bartosz@gmail.com>
2022-06-28 10:06:51 +0200

    registry and login

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -32,7 +32,7 @@ export default function HomePage() {
       
                 <a
                   className="inline-block px-12 py-3 mt-8 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring"
-                  href="/register"
+                  href="/login"
                 >
                   Zacznij korzystać !
                 </a>
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,107 +1,111 @@
-import React from 'react'
-import started from '../../img/getStarted.jpg'
+import React from 'react';
 
 export default function RegisterPage() {
-  return (
+	return (
+		<div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+			<div className="max-w-lg mx-auto">
+				<h1 className="text-2xl font-bold text-center text-indigo-600 sm:text-3xl">
+					Get started today
+				</h1>
 
-<section className="relative flex flex-wrap lg:h-screen lg:items-center">
-  <div className="w-full px-4 py-12 lg:w-1/2 sm:px-6 lg:px-8 sm:py-16 lg:py-24">
-    <div className="max-w-lg mx-auto text-center">
-      <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+				<p className="max-w-md mx-auto mt-4 text-center text-gray-500">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+					sunt dolores deleniti inventore quaerat mollitia?
+				</p>
 
-      <p className="mt-4 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla
-        eaque error neque ipsa culpa autem, at itaque nostrum!
-      </p>
-    </div>
+				<form
+					action=""
+					className="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl"
+				>
+					<p className="text-xl text-center font-medium">
+						Sign in to your account
+					</p>
 
-    <form action="" className="max-w-md mx-auto mt-8 mb-0 space-y-4">
-      <div>
-        <label for="email" className="sr-only">Email</label>
+					<div>
+						<label for="email" className="text-sm font-medium">
+							Email
+						</label>
 
-        <div className="relative">
-          <input
-            type="email"
-            className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-            placeholder="Enter email"
-          />
+						<div className="relative mt-1">
+							<input
+								type="email"
+								id="email"
+								className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+								placeholder="Enter email"
+							/>
 
-          <span className="absolute inset-y-0 inline-flex items-center right-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
-          </span>
-        </div>
-      </div>
+							<span className="absolute inset-y-0 inline-flex items-center right-4">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="w-5 h-5 text-gray-400"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+									/>
+								</svg>
+							</span>
+						</div>
+					</div>
 
-      <div>
-        <label for="password" className="sr-only">Password</label>
-        <div className="relative">
-          <input
-            type="password"
-            className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
-            placeholder="Enter password"
-          />
+					<div>
+						<label for="password" className="text-sm font-medium">
+							Password
+						</label>
 
-          <span className="absolute inset-y-0 inline-flex items-center right-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-              />
-            </svg>
-          </span>
-        </div>
-      </div>
+						<div className="relative mt-1">
+							<input
+								type="password"
+								id="password"
+								className="w-full p-4 pr-12 text-sm border-gray-200 rounded-lg shadow-sm"
+								placeholder="Enter password"
+							/>
 
-      <div className="flex items-center justify-between">
-        <p className="text-sm text-gray-500">
-          No account?
-          <a className="underline" href="">Sign up</a>
-        </p>
+							<span className="absolute inset-y-0 inline-flex items-center right-4">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="w-5 h-5 text-gray-400"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+									/>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+									/>
+								</svg>
+							</span>
+						</div>
+					</div>
 
-        <button
-          type="submit"
-          className="inline-block px-5 py-3 ml-3 text-sm font-medium text-white bg-blue-500 rounded-lg"
-        >
-          Sign in
-        </button>
-      </div>
-    </form>
-  </div>
+					<button
+						type="submit"
+						className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg"
+					>
+						Sign up
+					</button>
 
-  <div className="relative w-full h-64 sm:h-96 lg:w-1/2 lg:h-full">
-    <img
-      className="absolute inset-0 object-cover w-full h-full"
-      src={started}
-      alt=""
-    />
-  </div>
-</section>
-  )
+					<p className="text-sm text-center text-gray-500">
+						Masz już konto ?
+						<a className="underline" href="/login">
+							Zaloguj się
+						</a>
+					</p>
+				</form>
+			</div>
+		</div>
+	);
 }
