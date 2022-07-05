8c1d8942c21399ecf04050b9b8f6d738cfa2edfc
Bartek Konarowski <konarowski.bartosz@gmail.com>
2022-07-05 10:57:54 +0200

    Data selec 1st stage

--- src/components/AddShopping/AddShopping.tsx
+++ src/components/AddShopping/AddShopping.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from 'src/config/firebase-config';
-import money from '../../img/money.jpg'
+import money from '../../img/money.jpg';
+import style from '../AddShopping/AddShopping.module.css';
+
 // Add a new document in collection "cities"
 
 export default function AddShopping() {
@@ -29,7 +31,7 @@ export default function AddShopping() {
 					<img
 						className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-85 sm:opacity-100"
 						src={money}
-						alt="Couple on a bed with a dog"
+						alt="savings pig and some coins"
 					/>
 
 					<div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent"></div>
@@ -37,20 +39,40 @@ export default function AddShopping() {
 					<div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
 						<div className="max-w-xl justify-center sm:text-left">
 							<h1 className="text-3xl font-extrabold sm:text-5xl">
-								Wyznacz nowy cel i zacznij oszczędzać		
+								Wyznacz nowy cel i zacznij oszczędzać
 							</h1>
 
 							<p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
-								Wybierz zakres dat i kontroluj wydatki, aby zrealizować plany krok po kroku. 
+								Wybierz zakres dat i kontroluj wydatki, aby zrealizować plany
+								krok po kroku.
 							</p>
 
 							<div className="flex mt-8 justify-left">
-
-								<button
-									className="px-5 py-2.5 text-sm font-medium text-white bg-indigo-600 rounded-md shadow"
-
-								>
-									Wybierz daty 
+								<button className="px-5 py-2.5 text-sm font-medium text-white bg-indigo-600 rounded-md shadow hover:bg-blue-700">
+									Wybierz daty
+								</button>
+							</div>
+							<div className={style.date_form}>
+								<form className={style.form_general}>
+									<label htmlFor="startDate">
+										<h6>Start date:</h6>
+									</label>
+									<input
+										className={style.form_input}
+										id="startDate"
+										type="date"
+									/>
+									<label htmlFor="endDate">
+										<h6>End date:</h6>
+									</label>
+									<input
+										className={style.form_input}
+										id="endDate"
+										type="date"
+									/>
+								</form>
+								<button className=" mt-1.5 px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md shadow hover:bg-blue-700">
+									Wyślij
 								</button>
 							</div>
 						</div>
